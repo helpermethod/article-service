78a1bf15683a2aa97a5bf3d6e1578d94224a9f96
oliver <weiler@predic8.de>
2017-10-06 13:25:50 +0200

    Set server.port to 0

--- src/main/java/com/predic8/workshop/entity/Article.java
+++ src/main/java/com/predic8/workshop/entity/Article.java
@@ -1,7 +1,6 @@
 package com.predic8.workshop.entity;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +8,6 @@ import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
-@NoArgsConstructor
 @Entity
 public class Article {
 	@Id
