0a75e0565a7f6e9135c7c0d32664c4ff8ee2bf70
oliver <weiler@predic8.de>
2017-10-06 11:45:38 +0200

    Add update action

--- src/main/java/com/predic8/workshop/rest/ArticleRestController.java
+++ src/main/java/com/predic8/workshop/rest/ArticleRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,13 @@ public class ArticleRestController {
 		URI uri = uriComponentsBuilder.path("/{id}").buildAndExpand(articleService.save(article).getId()).toUri();
 
 		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody Article article) {
+		articleService.update(id, article);
+
+		return ResponseEntity.noContent().build();
 	}
 
 	@DeleteMapping("/{id}")
--- src/main/java/com/predic8/workshop/service/ArticleService.java
+++ src/main/java/com/predic8/workshop/service/ArticleService.java
@@ -10,29 +10,39 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.UUID.randomUUID;
+
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
 	private final KafkaTemplate<String, ArticleDto> kafkaTemplate;
 	private final ArticleRepository articleRepository;
 
-	public Article save(Article article) {
-		Article savedArticle = articleRepository.save(article);
-
-		kafkaTemplate.send("articles", new ArticleDto("create", savedArticle.getName(), savedArticle.getDescription(), article.getPrice()));
-
-		return savedArticle;
-	}
-
 	public List<Article> index() {
 		return articleRepository.findAll();
 	}
 
-	public void delete(Long id) {
-		articleRepository.delete(id);
-	}
-
 	public Optional<Article> show(Long id) {
 		return Optional.ofNullable(articleRepository.findOne(id));
+	}
+
+	public Article save(Article article) {
+		Article savedArticle = articleRepository.save(article);
+
+		kafkaTemplate.send("articles", randomUUID().toString(), new ArticleDto("create", savedArticle.getName(), savedArticle.getDescription(), article.getPrice()));
+
+		return savedArticle;
+	}
+
+	public void update(Long id, Article article) {
+		article.setId(id);
+
+		Article savedArticle = articleRepository.save(article);
+
+		kafkaTemplate.send("articles", randomUUID().toString(), new ArticleDto("update", savedArticle.getName(), savedArticle.getDescription(), article.getPrice()));
+	}
+
+	public void delete(Long id) {
+		articleRepository.delete(id);
 	}
 }
